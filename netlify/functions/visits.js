122f99bd6ca092984f480287424f7b3bcf3bd5b9
lenthemann <120011578+lenthemann@users.noreply.github.com>
2026-02-12 17:24:17 +0100

    Update visits.js

--- netlify/functions/visits.js
+++ netlify/functions/visits.js
@@ -1,7 +1,26 @@
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ test: "It works" }),
-  };
-}
+export async function handler(event, context) {
+  try {
+    const PLACE_ID = "115442728708640";
 
+    const universeRes = await fetch(
+      `https://apis.roblox.com/universes/v1/places/${PLACE_ID}/universe`
+    );
+    const universeData = await universeRes.json();
+    const universeId = universeData.universeId;
+
+    const gameRes = await fetch(
+      `https://games.roblox.com/v1/games?universeIds=${universeId}`
+    );
+    const gameData = await gameRes.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(gameData.data[0]),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.toString() }),
+    };
+  }
+}
